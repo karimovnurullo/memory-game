349d50122ccb45487b0a31ea2c045222a349eaef
Nurullo <karimovdeveloper@gmail.com>
2023-06-24 01:20:14 +0500

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import { Board } from "./components";
+import { Card } from "./components";
+// Img1,Img2,Img3,Img4,Img5,Img6,Img7,Img8,Img9,Img10,
+import classes from "./assets/css/board.module.scss";
+import { useState } from "react";
+interface Image {
+  src: string;
+}
+
+interface CardData extends Image {
+  id: number;
+}
 
 function App() {
+  const [cards, setCards] = useState<CardData[]>([]);
+  const [turn, setTurn] = useState(0);
+
+  const images: Image[] = [
+    { src: "1" },
+    { src: "2" },
+    { src: "3" },
+    { src: "4" },
+    { src: "5" },
+    { src: "6" },
+    { src: "7" },
+    { src: "8" },
+    { src: "9" },
+    { src: "10" },
+  ];
+  let arr = [...images, ...images]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }));
+  // setCards(arr);
+  // setTurn(0);
+
+  const hundleCards = () => {};
+  const handleCardClick = () => {
+    console.log("Card clicked");
+  };
   return (
-    <div className="app">
-      <Board/>
+    <div className={classes.board}>
+      {arr.map(({ src, id }, idx) => (
+        <Card card={handleCardClick} src={src} flipped={true} key={id} />
+      ))}
+      {/* <Card card={handleCardClick} src={"1"} flipped={true} /> */}
     </div>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,34 @@
+import React, { useState } from "react";
+import classes from "../assets/css/card.module.css";
+import "../assets/css/main.css";
+import cover from "../assets/images/cover.jpg";
 
-const Card = () => {
-  return (
-    <div className="">Card</div>
-  )
+interface CardProps {
+  card: () => void;
+  src: string;
+  flipped: boolean;
 }
 
-export default Card
+const Card: React.FC<CardProps> = ({ card, flipped, src }) => {
+  const [active, setActive] = useState("");
+  // let img = `../assets/images/${src}.jpg`;
+  let img = require(`../assets/images/${src}.jpg`);
+
+  const handleClick = () => {
+    card();
+    setActive("flipped");
+    setTimeout(() => {
+      setActive("");
+    }, 1000);
+  };
+  return (
+    <div className={`item`} onClick={handleClick}>
+      <div className={`${active}`}>
+        <img src={img} className="cover" alt="" />
+        <img src={cover} className="main-img" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
